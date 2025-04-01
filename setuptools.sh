90acde6566ca83187c362936060e505a990e0be9
Natasha <tashageebs@gmail.com>
2025-04-01 16:40:10 -0400

    Add files via upload

---
+++ setuptools.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+# Update and upgrade the system
+sudo apt update && sudo apt upgrade -y
+
+# Install essential packages
+sudo apt install -y software-properties-common net-tools git nano vim golang autotools-dev build-essential libpcap-dev pipx python3-pip libtool shtool pkg-config wget ethtool rfkill libssl-dev libgcrypt20-dev libnl-3-dev libnl-genl-3-dev mono-complete
+
+# Install additional dependencies
+sudo apt update && sudo apt install -y build-essential git libmicrohttpd-dev libnl-3-dev libnl-genl-3-dev libcap-dev libpcap-dev libsqlite3-dev libprotobuf-dev protobuf-compiler libncurses-dev libssl-dev pkg-config
+sudo apt-get install -y libwebsockets-dev libusb-1.0-0-dev librtlsdr-dev
+sudo apt install -y tshark nmap python3-scapy
+
+# Clone and setup Exploit-Database
+git clone https://gitlab.com/exploit-database/exploitdb.git /opt/exploit-database
+ln -sf /opt/exploit-database/searchsploit /usr/local/bin/searchsploit
+cp -n /opt/exploit-database/.searchsploit_rc ~/
+
+# Clone and build arp-scan
+git clone https://github.com/royhills/arp-scan.git
+cd arp-scan
+autoreconf --install
+./configure
+make
+make check
+make install
+cd ..
+
+# Clone and setup sqlmap
+git clone --depth 1 https://github.com/sqlmapproject/sqlmap.git sqlmap-dev
+
+# Install gvm-tools via pipx
+python3 -m pipx install gvm-tools
+echo 'export PATH=$PATH:/root/.local/bin' >> ~/.bashrc
+source ~/.bashrc
+
+# Clone and setup Social-Engineer Toolkit (SET)
+git clone https://github.com/trustedsec/social-engineer-toolkit
+
+# Download and install aircrack-ng
+wget https://download.aircrack-ng.org/aircrack-ng-1.7.tar.gz
+tar -zxvf aircrack-ng-1.7.tar.gz
+cd aircrack-ng-1.7
+autoreconf -i
+./configure --with-experimental
+make
+make install
+ldconfig
+cd ..
+
+# Download and install Snaffler
+wget https://github.com/SnaffCon/Snaffler/releases/latest/download/Snaffler.exe -O Snaffler.exe
+
+# Setup Kismet repository and install Kismet
+wget -O - https://www.kismetwireless.net/repos/kismet-release.gpg.key --quiet | gpg --dearmor | sudo tee /usr/share/keyrings/kismet-archive-keyring.gpg >/dev/null
+echo 'deb [signed-by=/usr/share/keyrings/kismet-archive-keyring.gpg] https://www.kismetwireless.net/repos/apt/git/noble noble main' | sudo tee /etc/apt/sources.list.d/kismet.list >/dev/null
+sudo apt update
+sudo apt install -y kismet
+
+# Install John the Ripper
+sudo apt-get install -y john
+
+# Clone and setup Responder
+git clone https://github.com/lgandx/Responder.git
+cd Responder
+sudo apt install -y python3-netifaces
+cd ..
+
+# Install Hydra
+sudo apt install -y hydra
+
+# Install Metasploit
+curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall
+chmod 755 msfinstall
+./msfinstall
+
+# Clone and setup Veil
+git clone https://github.com/Veil-Framework/Veil.git
+cd Veil/
+./config/setup.sh --force --silent
+cd ..
+
+# Clone and build ScareCrow
+git clone https://github.com/Tylous/ScareCrow.git
+cd ScareCrow
+go mod init github.com/Tylous/ScareCrow
+go mod tidy
+go build
+./ScareCrow --help
+cd ..
+
+# Clone and build Freeze
+git clone https://github.com/Tylous/Freeze.git
+cd Freeze
+go mod init freeze
+go mod tidy
+go build -o Freeze Freeze.go
+cd ..
+
+# Install Netcat
+sudo apt install -y netcat-openbsd
+
+sudo apt update && sudo apt upgrade -y
+
+echo "All commands executed successfully!"
